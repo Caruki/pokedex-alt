d39817cd9040af248f5f4d0eb9f0d454c4728c01
Caruki <caruki91@posteo.de>
2020-03-02 17:38:26 +0100

    Add search component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import './index.scss';
 
 import { title } from './components/title';
+import { search } from './components/search';
 
 document.body.appendChild(title('Pokdex'));
+document.body.appendChild(search());
