2e48ab856051e2d139193e9f7fcf285f8c16e2a8
Caruki <caruki91@posteo.de>
2020-03-04 14:20:29 +0100

    Read and write searchValue from sessionStorage on page load
    Add external filter function

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import './components/search.scss';
 import { createElement, appendContent } from './lib/dom';
 import { createTitle } from './components/title';
 import { createSearchInput, createSearchResults } from './components/search';
+import { filterResults } from './components/filter';
 import Logo from './assets/logo.svg';
 
 const cardList = ['Nissa', 'Ajani', 'Archon', 'Eidolon'];
@@ -13,27 +14,28 @@ export function app() {
   });
   const main = createElement('main', { className: 'main' });
   const title = createTitle('MagicDex', { className: 'title' });
-  // const searchValueInput = sessionStorage.getItem('searchValue');
   const searchInput = createSearchInput(sessionStorage.getItem('searchValue'));
   const logo = createElement('img', { src: Logo, className: 'logo' });
-  let magicCards = createSearchResults(cardList);
+
+  let magicCards = null;
+
+  function setSearchResults() {
+    magicCards = createSearchResults(
+      filterResults(searchInput.value, cardList)
+    );
+    appendContent(main, magicCards);
+  }
+  setSearchResults();
 
   appendContent(header, [logo, title]);
   appendContent(main, [searchInput, magicCards]);
 
   searchInput.addEventListener('input', event => {
     main.removeChild(magicCards);
+    setSearchResults();
+
     const searchValue = event.target.value;
-    const lowerCaseSearchValue = event.target.value.toLowerCase();
-    const filteredCards = cardList.filter(card => {
-      if (searchValue.length > 0) {
-        return card.toLowerCase().includes(lowerCaseSearchValue);
-      }
-    });
-    magicCards = createSearchResults(filteredCards);
-    appendContent(main, magicCards);
     sessionStorage.setItem('searchValue', searchValue);
-    // console.log(sessionStorage.getItem('searchValue'));
   });
 
   return [header, main];
