2807673ea136f3db77b7cea2cff01326315e18ee
Caruki <caruki91@posteo.de>
2020-03-04 10:15:55 +0100

    Add logo to header

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import './components/search.scss';
 import { createElement, appendContent } from './lib/dom';
 import { createTitle } from './components/title';
 import { createSearchInput, createSearchResults } from './components/search';
+import Logo from './assets/logo.svg';
 
 const cardList = ['Nissa', 'Ajani', 'Archon', 'Eidolon'];
 
@@ -13,9 +14,10 @@ export function app() {
   const main = createElement('main', { className: 'main' });
   const title = createTitle('MagicDex', { className: 'title' });
   const searchInput = createSearchInput();
+  const logo = createElement('img', { src: Logo, className: 'logo' });
   let magicCards = createSearchResults(cardList);
 
-  appendContent(header, [title]);
+  appendContent(header, [logo, title]);
   appendContent(main, [searchInput, magicCards]);
 
   searchInput.addEventListener('search', event => {
