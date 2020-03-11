9b35d86122f3c46745f70ad4dd4650705459a745
Carmen Seeba <60884245+Caruki@users.noreply.github.com>
2020-03-11 09:50:31 +0100

    Flex structure (#22)
    * Change div container structure
    
    * Edit flex structure
    
    * Fix flexbox and adjust styling
    
    * Fix favouritelist behaviour

--- src/components/search.js
+++ src/components/search.js
@@ -21,26 +21,3 @@ export function createShowAllButton() {
   });
   return element;
 }
-
-export function createSearchResults(props) {
-  const container = createElement('div', { className: 'listContainer' });
-  if (props.results.length === 0) {
-    const notFound = createElement('div', {
-      innerText: 'No Result matching found'
-    });
-    appendContent(container, notFound);
-  } else {
-    props.results.forEach(item => {
-      const element = createElement('div', {
-        innerText: item,
-        className: 'resultItem'
-      });
-      element.addEventListener('click', () => {
-        props.onSearchResultClick(item);
-      });
-
-      appendContent(container, element);
-    });
-  }
-  return container;
-}
